825d6ab6327e82c158614e04f05832fdd533df4f
Andre Gustavo <andre.gustavo@joinplank.com>
2024-02-16 10:38:52 -0300

    adding architecture test

--- src/tests/arch-tests/clean-arch.test.ts
+++ src/tests/arch-tests/clean-arch.test.ts
@@ -1,107 +1,104 @@
-import "tsarch/dist/jest"
-import {filesOfProject} from "tsarch"
-
+import "tsarch/dist/jest";
+import {filesOfProject} from "tsarch";
 
 describe("Given the Entities layer", ()=> {
-    jest.setTimeout(60000);
+  jest.setTimeout(60000);
 
-    it("Should not have dependencies from External in Entities", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("entities")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("external")
+  it("Should not have dependencies from External in Entities", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("entities")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("external");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from Controllers in Entities", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("entities")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("adapters")
+  it("Should not have dependencies from Controllers in Entities", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("entities")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("adapters");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from UseCases in Entities", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("entities")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("use-cases")
+  it("Should not have dependencies from UseCases in Entities", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("entities")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("use-cases");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from UseCases in Entities", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("entities")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("main")
+  it("Should not have dependencies from UseCases in Entities", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("entities")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("main");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 });
-
 
 describe("Given the UseCases layer", ()=> {
-    jest.setTimeout(60000);
+  jest.setTimeout(60000);
 
-    it("Should not have dependencies from External in UseCases", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("use-cases")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("external")
+  it("Should not have dependencies from External in UseCases", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("use-cases")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("external");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from Adapters in UseCases", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("use-cases")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("adapters")
+  it("Should not have dependencies from Adapters in UseCases", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("use-cases")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("adapters");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from UseCases in UseCases", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("use-cases")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("main")
+  it("Should not have dependencies from UseCases in UseCases", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("use-cases")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("main");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 });
 
-
 describe("Given the Adapters layer", ()=> {
-    jest.setTimeout(60000);
+  jest.setTimeout(60000);
 
-    it("Should not have dependencies from External in Adapters", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("adapters")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("external")
+  it("Should not have dependencies from External in Adapters", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("adapters")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("external");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 
-    it("Should not have dependencies from UseCases in Adapters", async ()=> {
-        const rule = filesOfProject()
-            .inFolder("adapters")
-            .shouldNot()
-            .dependOnFiles()
-            .inFolder("main")
+  it("Should not have dependencies from UseCases in Adapters", async ()=> {
+    const rule = filesOfProject()
+      .inFolder("adapters")
+      .shouldNot()
+      .dependOnFiles()
+      .inFolder("main");
 
-        await expect(rule).toPassAsync()
-    })
+    await expect(rule).toPassAsync();
+  });
 });
